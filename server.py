3598b223ea38f058a2af555eecb03905e64d472d
aayush kubitkar <kubitkar.a@husky.neu.edu>
2020-02-14 12:33:44 -0500

    implement authn and authz

--- server.py
+++ server.py
@@ -2,11 +2,11 @@ import socket
 import select
 import struct
 import nstp_v3_pb2
-#from nacl.public import PrivateKey, Box
 from nacl.bindings import crypto_kx, crypto_secretbox, crypto_secretbox_open
 from nacl import utils, secret
 from passlib.context import CryptContext
 import threading
+import hashlib
 
 def createSessionKey(s, client_pub_key):
     rx ,tx = crypto_kx.crypto_kx_server_session_keys(pk_server, sk_server, client_pub_key)
@@ -59,49 +59,114 @@ def handleEncryptedMessage(s, nstp_msg):
         print ("out of spec")
 
 def handleAuthReq(s, decr_msg):
-    if s.getpeername()[0] not in list_ip_ban.keys():
+    if list_ip_authenticated[s] == 0:
         auth_req = nstp_v3_pb2.AuthenticationRequest()
         auth_req.CopyFrom(decr_msg.auth_request)
         ctx = CryptContext(schemes=["md5_crypt", "sha256_crypt", "sha512_crypt"])
         pwhash=getHashFromFile(auth_req.username)
         if pwhash:
             if ctx.verify(auth_req.password, pwhash):
-                print ("authentication success")
-                list_ip_failed_logins[clientsocket.getpeername()[0]]=0
-                AuthRes(s, True)
+                print ("Authentication success")
+                list_ip_authenticated[s]=1 #Connections with value 1 are authenticated
+                conn_user_map[s]=auth_req.username #Used for private value store
+                list_ip_failed_logins[clientsocket.getpeername()[0]]=0 #resetting the failed login attempts
+                authRes(s, True)
             else:
-                print ("authentication failed")
+                print ("Authentication failed")
                 val=list_ip_failed_logins[s.getpeername()[0]]
                 list_ip_failed_logins[s.getpeername()[0]]=val+1
-                if (list_ip_failed_logins[s.getpeername()[0]] > 10)
+                if list_ip_failed_logins[s.getpeername()[0]] > 10:
                     handleRateLimit(s)
-                AuthRes(s, False)
+                authRes(s, False)
         else:
-            print ("user not found")
+            print ("User not found")
     else:
-        print ("IP blocked for too many attempts. Please try again after sometime.")
+        print ("User already authentiacted.")
 
-def AuthRes(s, decision):
+def authRes(s, decision):
     auth_res= nstp_v3_pb2.AuthenticationResponse()
     auth_res.authenticated=decision
+    EncryptAndSend(s,auth_res)
+
+def handlePingReq(s, decr_msg):
+    if s in list_ip_authenticated and list_ip_authenticated[s]==1:
+        ping_req = nstp_v3_pb2.PingRequest()
+        ping_req.CopyFrom(decr_msg.ping_request)
+        pingRes(s,ping_req)
+    else:
+        print("User is not authenticated")
+
+def pingRes(s, ping_req):
+    ping_res = nstp_v3_pb2.PingResponse()
+    if ping_req.hash_algorithm == HashAlgorithm.IDENTITY:
+        ping_res.hash = ping_req.data
+    elif ping_req.hash_algorithm == HashAlgorithm.SHA256:
+        ping_res.hash = hashlib.sha256(ping_req.data)
+    elif ping_req.hash_algorithm == HashAlgorithm.SHA512:
+        ping_res.hash = hashlib.sha512(ping_req.data)
+    else:
+        return
+    EncryptAndSend(s,ping_res)
+
+def handleStoreReq(s, decr_msg):
+    if s in list_ip_authenticated and list_ip_authenticated[s]==1:
+        store_req = nstp_v3_pb2.StoreRequest()
+        store_req.CopyFrom(decr_msg.store_request)
+        if store_req.public:
+            public_value_store[store_req.key]=store_req.value
+        else:
+            username = conn_user_map[s]
+            if username not in priv_value_store.keys():
+                priv_value_store[username]={store_req.key:store_req.value}
+            else:
+                priv_value_store[username].update({store_req.key:store_req.value})
+        storeRes(s, store_req.key, store_req.value)
+    else:
+        print("User is not authenticated")
+
+def storeRes(s, key, value):
+    store_res = nstp_v3_pb2.StoreResponse()
+    store_res.hash = hashlib.sha256(value)
+    store_res.hash_algorithm = HashAlgorithm.SHA256
+    EncryptAndSend(s, store_res)
+
+def handleLoadReq(s, decr_msg):
+    if s in list_ip_authenticated and list_ip_authenticated[s]==1:
+        load_req = nstp_v3_pb2.LoadRequest()
+        load_req.CopyFrom(decr_msg.load_request)
+        if load_req.public:
+            if load_req.key in public_value_store.keys():
+               loadres(s, public_value_store[load_req.key])
+            else:
+                print("Key doesn't exist in public store")
+        else:
+            username = conn_user_map[s]
+            if load_req.key in priv_value_store[username].keys():
+                loadRes(s, priv_value_store[username][load_req.key])
+            else:
+                print ("Unauthorized access to private key")
+    else:
+        print("User is not authenticated")
+
+def loadRes(s, val):
+    load_res = nstp_v3_pb2.LoadResponse()
+    load_res.value = val
+    EncryptAndSend(ls, load_res)
+
+def EncryptAndSend(s,res):
     nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
-    encr_auth_res=nstp_v3_pb2.EncryptedMessage()
-    encr_auth_res.ciphertext=crypto_secretbox(auth_res.SerializeToString(), nonce, dict_session_keys[s][1])
-    encr_auth_res.nonce=nonce
+    encr_res=nstp_v3_pb2.EncryptedMessage()
+    encr_res.ciphertext=crypto_secretbox(res.SerializeToString(), nonce, dict_session_keys[s][1])
+    encr_res.nonce=nonce
     nstp_res= nstp_v3_pb2.NSTPMessage()
-    nstp_res.encrypted_message.CopyFrom(encr_auth_res)
+    nstp_res.encrypted_message.CopyFrom(encr_res)
     response_message[s]= append_len(nstp_res.SerializeToString())
-# def handlePingReq():
-
-# def handleStoreReq():
-
-# def handleLoadReq():
 
 def unblockIP(s):
-    del list_ip_ban[s.getpeername()[0]]
+    list_ip_ban[s.getpeername()[0]]=0 #IPs with value 0 are allowed to authenticate
 
 def handleRateLimit(s):
-    list_ip_ban[s.getpeername()[0]]=1
+    list_ip_ban[s.getpeername()[0]]=1 #IPs with value 1 are banned
     t = threading.Timer(25, unblockIP, [s])
     t.start()
 
@@ -144,11 +209,13 @@ server.listen(5)
 incoming=[server]
 outgoing=[]
 client_init={}
-list_ip_rate_limit={}
+list_ip_authenticated={}
 list_ip_ban={}
 list_ip_failed_logins={}
 response_message={}
-value_store={}
+priv_value_store={}
+public_value_store={}
+conn_user_map={}
 dict_session_keys={}
 sk_server, pk_server = crypto_kx.crypto_kx_keypair()
 print (pk_server, len(pk_server))
@@ -160,9 +227,14 @@ while incoming:
         if s is server:
             print("connection accepted from", s)
             clientsocket, addr = server.accept()
-            list_ip_failed_logins[addr[0]]=0
-            clientsocket.setblocking(False)
-            incoming.append(clientsocket)
+            ip = clientsocket.getpeername()[0]
+            if ip in list_ip_ban.keys() and list_ip_ban[ip] == 1:
+                print ("IP is banned to connect. Please try again after sometime.")
+                clientsocket.close()
+            else:
+                list_ip_failed_logins[clientsocket.getpeername()[0]]=0
+                clientsocket.setblocking(False)
+                incoming.append(clientsocket)
         else:
             print("data read from socket", s)
             data=s.recv(2)
